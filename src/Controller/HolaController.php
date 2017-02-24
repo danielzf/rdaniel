9a4f5d86b28366e5f36358e61c8b3a042c7e4f8b
danielzf <daniel.feria.unam@hotmail.com>
2017-02-24 12:59:54 -0600

    first commit

---
+++ src/Controller/HolaController.php
@@ -0,0 +1,19 @@
+<?php
+	namespace Drupal\hola\Controller;
+	class HolaController {
+	public function mensaje() {
+        	return array(
+               	 	'#title'  => 'Hola mundo!',
+                	'#markup' => 'Mi primer modulo en drupal 8',
+            	);
+    }
+}
+
+
+
+
+
+
+
+
+
